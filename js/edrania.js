a8afad509d620c0f3b31f1edc029bcf8c1cc2d93
Johan Nygren <johan.nygren91@gmail.com>
2020-11-02 22:38:00 +0100

    Filter for level in team games

--- js/edrania.js
+++ js/edrania.js
@@ -18,15 +18,15 @@ class Auction
 		const $input = $(event.currentTarget);
 		const value = $input.val();
 
-		$('.compact-table 	tbody').find('tr').each(function(){
+		$('.compact-table tbody tr').each(function(){
 			const $tr = $(this);
 
 			const name = $tr.find('td:first a').text();
 			if (name.search(new RegExp(value, 'i')) === -1) {
-				$tr.css('display', 'none');
+				$tr.hide();
 			}
 			else {
-				$tr.css('display', 'table-row');
+				$tr.show();
 			}
 		});
 	}
@@ -34,18 +34,19 @@ class Auction
 
 class TeamGame
 {
-	constructor()
+	constructor(action)
 	{
-		this.bindEvents();
-		this.prefillInputs();
-	}
+		switch (action) {
+			case 'create':
+				break;
 
-	/**
-	 * Bind events to inputs
-	 */
-	bindEvents()
-	{
+			case 'list':
+				this.setupFilter();		
+				break;
+		}
+
 		$('input, select').on('change', (event) => {this.savePrefill(event)});
+		this.prefillInputs();
 	}
 
 	getPrefill()
@@ -116,14 +117,74 @@ class TeamGame
 			$select.val(value).trigger('change');
 		});
 	}
+
+	/**
+	 * Setup filter for list
+	 */
+	setupFilter()
+	{
+		// Add filter for hiding games that are too high or too low level for the player
+		const $input = $('<input class="js-list-filter" type="checkbox" name="hideOutOfLevel">');
+		$input.on('change', (event) => {this.filterList(event)});
+
+		const $label = $('<label>');
+		$label.append($input).append(' Dölj spel utanför din grad');
+
+		$('.compact-table').before($label);
+	}
+
+	/**
+	 * Filter game lsit
+	 */
+	filterList(event)
+	{
+		$('.js-list-filter').each(function(){
+			let $input = $(this);
+
+			switch ($input.attr('name')) {
+				case 'hideOutOfLevel':
+					if ($input.is(':checked')) {
+						$('.compact-table tbody tr').each(function(){
+							const $tr = $(this);
+
+							const levels = $tr.find('td:nth(2)').text().split(' - ');
+							const playerLevel = getPlayerLevel();
+
+							if (parseInt(levels[0]) > playerLevel || parseInt(levels[1]) < playerLevel) {
+								$tr.hide();
+							}
+							else {
+								$tr.show();
+							}
+						});
+					}
+					else {
+						$('.compact-table tbody tr').show();
+					}
+					break;
+			}
+		});
+	}
+}
+
+/**
+ * Get current level of player
+ * @return {int}
+ */
+function getPlayerLevel() {
+	return parseInt($('#gladStatus table tbody tr:nth(1) td').text());
 }
 
 switch (location.pathname) {
 	case '/Auction':
-		const auktion = new Auction();
+		new Auction();
+		break;
+
+	case '/TeamGame/':
+		new TeamGame('list');
 		break;
 
 	case '/TeamGame/Create':
-		const geamGame = new TeamGame();
+		new TeamGame('create');
 		break;
 }
