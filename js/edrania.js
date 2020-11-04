d924f5858b18d9b558ad07abe7b8be7713162bd7
Johan Nygren <johan.nygren91@gmail.com>
2020-11-04 18:02:54 +0100

    Added prefill class

--- js/edrania.js
+++ js/edrania.js
@@ -45,77 +45,8 @@ class TeamGame
 				break;
 		}
 
-		$('input, select').on('change', (event) => {this.savePrefill(event)});
-		this.prefillInputs();
-	}
-
-	getPrefill()
-	{
-		let teamGamePrefill = localStorage.getItem('teamGamePrefill');
-
-		if (teamGamePrefill === null) {
-			teamGamePrefill = {};
-		}
-		else {
-			teamGamePrefill = JSON.parse(teamGamePrefill);
-		}
-
-		return teamGamePrefill;
-	}
-
-	/**
-	 * Save to local storage
-	 */
-	savePrefill(event)
-	{
-		const $input = $(event.currentTarget);
-		let teamGamePrefill = this.getPrefill();
-
-		if ($input.is(':checkbox')) {
-			teamGamePrefill[$input.attr('name')] = $input.is(':checked') ? true : false;
-		}
-		else {
-			teamGamePrefill[$input.attr('name')] = $input.val();
-		}
-
-		localStorage.setItem('teamGamePrefill', JSON.stringify(teamGamePrefill));
-	}
-
-	/**
-	 * Prefill inputs from local storage
-	 */
-	prefillInputs()
-	{
-		const teamGamePrefill = this.getPrefill();
-
-		$('input').each(function(){
-			let $input = $(this);
-
-			let value = teamGamePrefill[$input.attr('name')];
-			if (value === undefined) {
-				return;
-			}
-
-			if ($input.is(':checkbox')) {
-				if (value) {
-					$input.trigger('click');
-				}
-			}
-			else {
-				$input.val(value);
-			}
-		});
-
-		$('select').each(function(){
-			let $select = $(this);
-
-			let value = teamGamePrefill[$select.attr('name')];
-			if (value === undefined) {
-				return;
-			}
-
-			$select.val(value).trigger('change');
-		});
+		$('input, select').on('change', (event) => {prefillClass.savePrefill('teamGamePrefill', event)});
+		prefillClass.prefillInputs('teamGamePrefill');
 	}
 
 	/**
@@ -174,6 +105,8 @@ class TeamGame
 function getPlayerLevel() {
 	return parseInt($('#gladStatus table tbody tr:nth(1) td').text());
 }
+
+prefillClass = new Prefill();
 
 if (location.pathname === '/Auction') {
 	new Auction();
---
+++ js/prefill.js
@@ -0,0 +1,74 @@
+class Prefill
+{
+	/**
+	 * Get prefill
+	 */
+	getPrefill(itemName)
+	{
+		let prefillData = localStorage.getItem(itemName);
+
+		if (prefillData === null) {
+			prefillData = {};
+		}
+		else {
+			prefillData = JSON.parse(prefillData);
+		}
+
+		return prefillData;
+	}
+
+	/**
+	 * Save to local storage
+	 */
+	savePrefill(itemName, event)
+	{
+		const $input = $(event.currentTarget);
+		let prefillData = this.getPrefill(itemName);
+		
+		if ($input.is(':checkbox')) {
+			prefillData[$input.attr('name')] = $input.is(':checked') ? true : false;
+		}
+		else {
+			prefillData[$input.attr('name')] = $input.val();
+		}
+
+		localStorage.setItem(itemName, JSON.stringify(prefillData));
+	}
+
+	/**
+	 * Prefill inputs from local storage
+	 */
+	prefillInputs(itemName)
+	{
+		const prefillData = this.getPrefill(itemName);
+
+		$('input').each(function(){
+			let $input = $(this);
+
+			let value = prefillData[$input.attr('name')];
+			if (value === undefined) {
+				return;
+			}
+
+			if ($input.is(':checkbox')) {
+				if (value) {
+					$input.trigger('click');
+				}
+			}
+			else {
+				$input.val(value);
+			}
+		});
+
+		$('select').each(function(){
+			let $select = $(this);
+
+			let value = prefillData[$select.attr('name')];
+			if (value === undefined) {
+				return;
+			}
+
+			$select.val(value).trigger('change');
+		});
+	}
+}
