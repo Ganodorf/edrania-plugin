dc46cccacf0ad6c4b8846463460a6e1c6275377f
Johan Nygren <johan.nygren@wikinggruppen.se>
2020-11-05 10:57:43 +0100

    Display how much hp each threshold is in give up select

--- js/edrania.js
+++ js/edrania.js
@@ -6,6 +6,24 @@ function getPlayerLevel() {
 	return parseInt($('#gladStatus table tbody tr:nth(1) td').text());
 }
 
+/**
+ * Get players max HP
+ * @return {int}
+ */
+function getPlayerMaxHP() {
+	const hp = $('#gladStatus table tbody tr:nth(0) td').text();
+	const hpArr = hp.split('/');
+	return parseInt(hpArr[1]);
+}
+
+// Display how much hp each threshold is
+const playerHP = getPlayerMaxHP();
+$('select[name=RetreatThreshold] option').each(function(){
+	const value = parseInt($(this).val());
+	const thresholdHP = parseInt(playerHP * (value / 100));
+	$(this).append(' (' + thresholdHP + ' hp)');
+});
+
 chrome.storage.sync.get('edraniaConfig', function(data){
 	edraniaConfig = data.edraniaConfig;
 	if (edraniaConfig === undefined) {
