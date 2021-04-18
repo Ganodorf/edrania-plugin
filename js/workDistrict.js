610f64c625d8c194ba1dac2b98e91f4f250a67a7
Oskar Löfgren <oskar.lofgren@aquarobur.com>
2021-04-18 22:32:24 +0200

    [feat] Work district: duel friendly jobs and default rounds

--- js/workDistrict.js
+++ js/workDistrict.js
@@ -3,6 +3,8 @@ class WorkDistrict
 	constructor()
 	{
 		this.displaySalaryPerShift();
+		this.initDefaultSelectedRounds();
+		this.initDuelFriendlyJobs();
 
 		$('#Time').focus();
 	}
@@ -15,5 +17,50 @@ class WorkDistrict
 				` (${Math.floor(parseInteger($option.text()) * 0.28)} sm)`
 			);
 		})
+	}
+
+	initDefaultSelectedRounds()
+	{
+		if (!edraniaConfig.workshopSetDefaultRounds) {
+			return;
+		}
+
+		const playerRounds = getPlayerTime();
+
+		if (playerRounds >= edraniaConfig.workshopDefaultRounds) {
+			$('#Time').val(edraniaConfig.workshopDefaultRounds);
+		}
+	}
+
+	initDuelFriendlyJobs()
+	{
+		if (!edraniaConfig.workshopDuelFriendlyJobs) {
+			return;
+		}
+
+		const playerRounds = getPlayerTime();
+		const $rounds = $('#Time');
+		const $selectedOption = $rounds.find('option:selected');
+		const selectedIndex = $selectedOption.index();
+
+		if (playerRounds === $selectedOption.val()) {
+			const $newOption = $rounds.find(
+				`option:eq(${Math.max(selectedIndex - 1, 0)})`
+			);
+			$rounds.val($newOption.val());
+		}
+
+		const $useUpAllRoundsWarning = $('<p>', {
+			html: '<strong>OBS!</strong> Jobbet kommer förbruka <em>alla</em> dina rundor.',
+			class: 'text-error',
+			css: {display: 'none', marginTop: '0.25rem'}
+		});
+
+		$rounds.after($useUpAllRoundsWarning);
+		$useUpAllRoundsWarning.toggle($rounds.val() === playerRounds);
+
+		$rounds.on('change', () => {
+			$useUpAllRoundsWarning.toggle($rounds.val() === playerRounds);
+		});
 	}
 }
